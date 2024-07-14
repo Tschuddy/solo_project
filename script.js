80a99dd7ddabcc6e5aaca612f5250c151cf59843
Tschuddy <141195843+Tschuddy@users.noreply.github.com>
2024-07-14 02:12:21 +0100

    LifeWARE.com

--- script.js
+++
@@ -1,7 +0,0 @@
-// Smooth scrolling for the "Get Started" button
-document.querySelector('a[href="#features"]').addEventListener('click', function(e) {
-  e.preventDefault();
-  document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-  });
-});
--- scripts.js
+++
@@ -1,9 +0,0 @@
-// Navbar shrink on scroll
-window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.navbar');
-  if (window.scrollY > 50) {
-      navbar.classList.add('scrolled');
-  } else {
-      navbar.classList.remove('scrolled');
-  }
-});
